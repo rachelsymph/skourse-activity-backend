17cfe11efb384a54e696bf210103ce85f77ab62b
carlos <cvelasco@skourse.com>
2022-02-24 20:47:10 +0800

    backend setup for get function

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/listings", require("./routes/listingRoutes"));
 
 app.listen(8000);
